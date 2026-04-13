5b9bf9e2a1f5a34c46df90181135eadf7a5b01ff
angelo <angelo.alves.2025111@my.istec.pt>
2026-04-13 14:50:49 +0100

    Commit 3: Criacao dos modelos e configuracao da base de dados

--- noticias/models.py
+++ noticias/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Artigo(models.Model):
+    titulo = models.CharField(max_length=200)
+    corpo_texto = models.TextField()
+
+    def __str__(self):
+        return self.titulo
+
+class Comentario(models.Model):
+    artigo = models.ForeignKey(Artigo, on_delete=models.CASCADE, related_name='comentarios')
+    texto = models.TextField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comentário em {self.artigo.titulo}"
